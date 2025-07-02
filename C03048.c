1b097eeda255fc3af0cfc79a6c3e29a6d852ce6b
Mai Hoàng Xuân <XuanMH.B22CN925@stu.ptit.edu.vn>
2025-07-01 17:17:11 -0700

    SỐ ƯU THẾ CHẴN

---
+++ C03048.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<string.h>
+
+#define print printf
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--){
+        char c[20];
+        scanf("%s", c);
+        int n = strlen(c), l = 0;
+        for(int i=0; i<n; i++){
+            if((c[i]-'0') % 2) l++;
+        }
+        print(l >= n / 2 ? "NO\n" : "YES\n");
+    }
+}
