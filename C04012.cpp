2eb24d939e37968b2bf69dabebdd79cffbd43972
Mai Hoàng Xuân <XuanMH.B22CN925@stu.ptit.edu.vn>
2025-07-11 02:48:11 -0700

    XUẤT HIỆN NHIỀU HƠN MỘT LẦN

---
+++ C04012.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
+#define ll long long
+#define print printf
+
+int cnt[1000006];
+
+int main() {
+	int t, n;
+	scanf("%d", &n);
+	int a[n];
+	for(int i=0; i<n; i++){
+		scanf("%d", &a[i]);
+		cnt[a[i]]++;
+	}
+	for(int i=0; i<n; i++){
+		if(cnt[a[i]] > 1){
+			print("%d ", a[i]);
+			cnt[a[i]] = 0;
+		}
+	}
+}
+
