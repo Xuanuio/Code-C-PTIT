7464c1d8328ae85afd127d216fc933f940869978
Mai Hoàng Xuân <XuanMH.B22CN925@stu.ptit.edu.vn>
2025-07-11 03:31:37 -0700

    DỊCH TRÁI

---
+++ C04020.cpp
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+#include<string.h>
+
+#define ll long long
+#define print printf
+
+int main() {
+	int n, m;
+	scanf("%d", &n);
+	int a[n];
+	for(int i=0; i<n; i++) scanf("%d", &a[i]);
+	scanf("%d", &m);
+	for(int i=m; i<n; i++) print("%d ", a[i]);
+	for(int i=0; i<m; i++) print("%d ", a[i]);
+}
+
