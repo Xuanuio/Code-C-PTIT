e7cd662a42bc2844772908b95d0cc6dd266abed6
Mai Hoàng Xuân <XuanMH.B22CN925@stu.ptit.edu.vn>
2025-07-24 02:51:29 -0700

    SỐ HOÀN HẢO NHỎ HƠN N

---
+++ C03008.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<math.h>
+
+#define print printf
+
+int perfect(int n){
+    int sum = 1;
+    for(int i=2; i<=sqrt(n); i++){
+        if(n % i == 0){
+            sum += i;
+            if(i != n / i) sum += n/i;
+        }
+    }
+    return sum == n;
+}
+
+int main(){
+    int n;
+    scanf("%d", &n);
+    for(int i=6; i<=n; i++){
+        if(perfect(i)) print("%d ", i);
+    }
+}
